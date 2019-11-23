2cbd69070c93d1fff758b87cd9c143e9c360e129
Granite Bagas <granitebagas28@gmail.com>
2019-11-24 00:09:29 +0700

    Add photo function to bootcamp

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
+const fileupload = require("express-fileupload");
+
 const connectDB = require("./config/db");
 const errorHandler = require("./middleware/error");
 
@@ -26,6 +29,12 @@ app.use(express.json());
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// File Uploading
+app.use(fileupload());
+
+// Set static folder
+app.use(express.static(path.join(__dirname, "public")));
 
 // Mount routers
 app.use("/api/v1/bootcamps", bootcamps);
