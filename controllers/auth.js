cfc374c8b261eb91db0ad1ec553670a2666c7066
Granite Bagas <granitebagas28@gmail.com>
2019-11-24 02:39:36 +0700

    Add sign in

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 
 /**
  * @desc		Register User
- * @route		GET /api/v1/auth/register
+ * @route		POST /api/v1/auth/register
  * @access	Public
  */
 
@@ -18,6 +18,43 @@ exports.register = asyncHandler(async (req, res, next) => {
     password,
     role
   });
+
+  // Create token
+  const token = user.getSignedJwtToken();
+
+  res.status(200).json({
+    success: true,
+    token
+  });
+});
+
+/**
+ * @desc		Login User
+ * @route		POST /api/v1/auth/login
+ * @access	Public
+ */
+
+exports.login = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // Validate email and password
+  if (!email || !password) {
+    return next(new ErrorResponse("Please provide an email and password", 400));
+  }
+
+  // Check for User
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+
+  // Check if password matches
+  const isMatch = await user.matchPassword(password);
+
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
 
   // Create token
   const token = user.getSignedJwtToken();
