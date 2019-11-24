7333ee61527b5ca33a7f46f59c633b0356bb1092
Granite Bagas <granitebagas28@gmail.com>
2019-11-24 23:50:19 +0700

    Add update user and password

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const {
   register,
   login,
   getMe,
+  updateDetails,
+  updatePassword,
   forgotPassword,
   resetPassword
 } = require("../controllers/auth");
@@ -14,6 +16,8 @@ const { protect } = require("../middleware/auth");
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
 router.post("/forgotpassword", forgotPassword);
 router.put("/resetpassword/:resettoken", resetPassword);
 
